628bcd117ea9d9b36708b2ba9fd1cec0b2677f72
Jim Rosser <jarosser06@gmail.com>
2015-01-28 20:57:44 -0600

    added solution to example1

--- example1/recipes/default.rb
+++ example1/recipes/default.rb
@@ -12,3 +12,22 @@
 # server 2.pool.ntp.org
 #
 # Make sure the service gets enabled and started
+#
+
+package 'ntp' do
+  action :install
+end
+
+ntp_servers = []
+3.times do |i|
+  ntp_servers.push "server #{i}.pool.ntp.org"
+end
+
+file '/etc/ntp.conf' do
+  action :create
+  content ntp_servers.join("\n")
+end
+
+service 'ntp' do
+  action [:enable, :start]
+end
